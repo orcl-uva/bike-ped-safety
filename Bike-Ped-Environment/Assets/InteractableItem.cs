dcdde10e08f66f54f5d31e2fc3aff7d0c0dcf1fa
orcluva <orcl@virginia.edu>
2019-04-16 16:10:53 -0400

    Added SteamVR assets

---
+++ Bike-Ped-Environment/Assets/InteractableItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour
+{
+    protected Rigidbody rigidbody;
+    protected bool originalState;
+    protected Transform originalParent;
+    private void Awake()
+    {
+        //rigidbody = GetComponent(typeof(Controller));
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
